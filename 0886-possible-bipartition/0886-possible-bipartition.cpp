a050144cd3738b0412821236ca9a5aeaaa0adf65
nocodeZ1122 <rkohli_be24@thapar.edu>
2026-08-14 00:18:30 +0530

    Time: 36 ms (58.28%), Space: 67.8 MB (97.93%) - LeetHub

---
+++ 0886-possible-bipartition/0886-possible-bipartition.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+
+    bool dfs(vector<int> adj[], int node, vector<int>& color, int currcolor)
+    {
+        color[node] = currcolor;
+
+        for(int neigh : adj[node])
+        {
+            // Same group -> impossible
+            if(color[neigh] == color[node])
+                return false;
+
+            // Not colored yet
+            if(color[neigh] == -1)
+            {
+                if(!dfs(adj, neigh, color, 1 - currcolor))
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
+    bool possibleBipartition(int n, vector<vector<int>>& dislikes)
+    {
+        vector<int> adj[n + 1];
+
+        // Build undirected graph
+        for(int i = 0; i < dislikes.size(); i++)
+        {
+            int u = dislikes[i][0];
+            int v = dislikes[i][1];
+
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        vector<int> color(n + 1, -1);
+
+        // Graph may have multiple components
+        for(int i = 1; i <= n; i++)
+        {
+            if(color[i] == -1)
+            {
+                if(!dfs(adj, i, color, 0))
+                    return false;
+            }
+        }
+
+        return true;
+    }
+};
